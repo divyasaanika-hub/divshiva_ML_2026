d8c23c7fd8e573115cd7fced8ef366fc5055aa07
Divya Shivamadaiah <divya.saanika@gmail.com>
2025-12-07 00:52:15 +0530

    Fix comment for clarity in hosting.py

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="tourism_project/deployment",     # the local folder containing your files
-    repo_id="divshiva1988/Tourism_package_acceptance_predictor_model",          # the target repo
+    repo_id="divshiva1988/Tourism_package_acceptance_predictor_model",          # the target repository
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
