3b50a7a6f8958f8489b9988e15fc655d68ef3eeb
Divya Shivamadaiah <divya.saanika@gmail.com>
2025-12-07 01:45:37 +0530

    Change repo type from 'space' to 'model'

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -5,6 +5,6 @@ api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="tourism_project/deployment",     # the local folder containing your files
     repo_id="divshiva1988/Tourism_package_acceptance_predictor_model",          # the target repository
-    repo_type="space",                      # dataset, model, or space
+    repo_type="model",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
